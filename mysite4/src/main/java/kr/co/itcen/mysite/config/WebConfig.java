015cfd48b543676f8097ec4dc6280de7bee5fa8d
venture21 <park0122@gmail.com>
2019-10-17 11:32:36 +0900

    document

--- mysite4/src/main/java/kr/co/itcen/mysite/config/WebConfig.java
+++ mysite4/src/main/java/kr/co/itcen/mysite/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
+import kr.co.itcen.config.web.FileuploadConfig;
 import kr.co.itcen.config.web.MVCConfig;
 import kr.co.itcen.config.web.MessageConfig;
 import kr.co.itcen.config.web.SecurityConfig;
